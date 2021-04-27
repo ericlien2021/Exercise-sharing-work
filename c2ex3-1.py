4652ae2f3d4e9a24f5b424af9f3e214d3e9d44fb
ericlien2021 <83092524+ericlien2021@users.noreply.github.com>
2021-04-27 13:02:03 -0700

    Add files via upload

---
+++ c2ex3-1.py
@@ -0,0 +1,5 @@
+album_year = 1981
+if(album_year < 1980) or (album_year == 1991) or (album_year == 1993):
+    print("This album came out in year %d" %album_year)
+else:
+    print("This ablum came out after 1980")
